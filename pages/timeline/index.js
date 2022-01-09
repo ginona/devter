4a8ba7b5a32332cfb8719b80e726907e17ba5935
ginona <ginonacchio94@gmail.com>
2022-01-09 20:34:55 -0300

    First part ended

---
+++ pages/timeline/index.js
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+
+export default function Timeline({ userName }) {
+    return (
+    <>
+    <h1>This is the timeline of { userName }!</h1>
+    <Link href='/'><a>Go home</a></Link>
+    <style jsx>{`
+    
+    `}
+    </style>
+    </>
+    )
+}
+
+Timeline.getInitialProps = () => {
+    return fetch('http://localhost:3000/api/hello')
+        .then(res => res.json())
+    }
